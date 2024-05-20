9136df55f8e5e3f5f9a421ab671f4cf15ac61758
divinchi8 <divineiroanya482@gmail.com>
2024-05-20 09:14:17 +0100

    new changes

---
+++ script.js
@@ -0,0 +1,382 @@
+// function myFunction2() {
+// let name = "Feyi";
+// let age = 41;
+// // alert("My name is " + name + " and I am " + age + " years old.");
+// alert(`My name is ${name} and I am ${age} years old.`);
+// }
+// function changeText() {
+// document.getElementById("text-to-change").innerHTML =
+// "We have a new paragraph.";
+// }
+// function hideText() {
+// document.getElementById("text-to-change").style.display = "none";
+// }
+// function showText() {
+// document.getElementById("text-to-change").style.display = "block";
+// }
+// function style2() {
+// document.getElementById("text-to-change").style.background = "yellow"
+// console.log("Background colour changed.")
+// }
+// // alert('This is my web page.');
+// console.log("This is my web page");
+// // JavaScript Statements
+// // Operators, Values, Expressions, Comments
+// // Semicolon
+// let a = 1;
+// let b = 6;
+// let c = a + b;
+// console.log(`The value of c is ${c}.`)
+// // JavaScript Syntax
+// // var, let, const
+// var d;
+// var e;
+// var f = 2 + 2;
+// var g = "abcdefghijklmnopqrstuvwxyz";
+// let h = "How na?";
+// const i = 23;
+// let name = "Iseoluwa";
+// // JavaScript Operators
+// // +, -, *, /
+// console.log(3 * 9 + f);
+// // JavaScript is case-sensitive.
+// // camelCase
+// let firstName = "Aaron";
+// console.log('firstName', firstName);
+// // lowercase
+// let lastname = "Odum";
+// console.log('lastname', lastname);
+// // UPPERCASE
+// let AGE = 6;
+// console.log('AGE', AGE);
+// // PascalCase
+// let TheNewAge = 7;
+// console.log('TheNewAge', TheNewAge);
+// // snake_case
+// let the_new_name = "Hannah";
+// console.log('the_new_name', the_new_name);
+// // JavaScript Variables
+// // JavaScript variables are containers for storing data.
+// x = 7;
+// const j = 11;
+// const k = 12;
+// let l = j + k;
+// function logVariable() {
+// newName = "Another Name";
+// // console.log('newName', newName)
+// }
+// console.log('newName', newName);
+// let time = "10am";
+// {
+// let time = "10am";
+// console.log('time', time)
+// }
+// {
+// console.log('newName', newName);
+// }
+// // var date = "11-01-2014";
+// // var date = "12-01-2014";
+// // date = "13-01-2014";
+// // let date = "11-01-2014";
+// // let date = "12-01-2014";
+// // date = "13-01-2014";
+// const date = "11-01-2014";
+// // const date = "12-01-2014";
+// date = "13-01-2014";
+// // var
+// // scope - No
+// // redeclare - Yes
+// // reassign - Yes
+// // let
+// // scope - Yes
+// // redeclare - No
+// // reassign - Yes
+// // const
+// // scope - Yes
+// // redeclare - No
+// // reassign - No
+// // console.log('date', date)
+// JavaScript Operators
+// addition operator
+console.log('addition', 2 + 2);
+// subtraction operator
+console.log('subtraction', 2 - 2);
+// multiplication operator
+console.log('multiplication', 2 * 2);
+// division operator
+console.log('division', 2 / 2);
+// modulus operator
+console.log('modulus', 5 % 2);
+// exponent operator
+console.log('exponent', 5 ** 2);
+let number = 5;
+let number2 = 10;
+// increment operator
+console.log('increment', ++number);
+console.log('increment', ++number);
+console.log('increment', ++number);
+// decrement operator
+console.log('decrement', number2--);
+console.log('decrement', number2--);
+console.log('decrement', number2--);
+// assignment operator
+let x = 2;
+let y = 5;
+console.log('assignment', x += 3); // x = x + 3;
+console.log('assignment', x -= 1); // x = x - 1;
+console.log('assignment', x *= 2); // x = x * 2;
+console.log('assignment', x /= 3); // x = x * 3;
+console.log('assignment', y %= 3); // y = y % 3;
+console.log('assignment', y **= 3); // y = y ** 3;
+// Comparison Operators
+// // == equal to
+// let a = 2;
+// let b = 3;
+// console.log(a == 2);
+// // === equal to (type and value)
+// let c = 4;
+// let d = "45";
+// console.log(c === d);
+// // != not equal to
+// let e = 2;
+// let f = 3;
+// console.log(e != f);
+// // !== not equal to (type and value)
+// let g = 2;
+// let h = "2";
+// console.log(g !== h);
+// // > greater than
+// let i = 3;
+// let j = 6;
+// console.log(i > j);
+// // < less than
+// let k = 3;
+// let l = 6;
+// console.log(k < l);
+// // >= greater than or equal to
+// let m = 3;
+// let n = 3;
+// console.log(m >= n);
+// // <= less than or equal to
+// let o = 3;
+// let p = 6;
+// console.log(o <= p);
+// let q = "Dim";
+// let r = "Dig"; // r = Dig3
+// console.log("Dig" + 3);
+// console.log("Dig" * 3);
+// console.log(q > r);
+// NaN
+// a, b, c, d
+// 1, 2, 3, 4
+// // String-Number Addition
+// let s = "F";
+// let t = 9;
+// console.log('s+t', s + t);
+// let u = "7";
+// let v = 7;
+// console.log('u+v', u + v);
+// Logical Operators
+// && logical and
+// || logcial or
+let a = 5;
+let b = 6;
+let c = 7;
+let d = 5;
+console.log(!(a < b));
+console.log(!(c < d));
+console.log(a < b && c < d && a == d); // true if all values are true
+console.log((a < b || c < d) && a == d); // true if one or more values are true
+// ! logical not
+let e = 6;
+console.log(!e);
+console.log(!true);
+// Data types
+// String, Number, Boolean, Null, Undefined, Symbol, Object, Bigint
+// String
+let string = "string";
+let myName = "Teslim";
+let string2 = `He said, "My name is ${myName}."`
+console.log(string2);
+// Number
+let number5 = 5;
+let total = "Hannah" + 3 + 5;
+let total2 = 3 + 5 + "Hannah";
+console.log('total', total);
+console.log('total2', total2);
+// Boolean
+// true or false
+let a3 = true; // Boolean
+let a4 = "false"; // String
+// Null
+let a5 = 10; // Null
+a5 = null;
+console.log(a5);
+// Undefined
+let a6;
+console.log('a6', a6);
+// Symbol
+let bee = "bee";
+let bee2 = "bee";
+console.log(bee === bee2);
+let bee3 = Symbol("bee");
+let bee4 = Symbol("bee");
+console.log(bee3 === bee4);
+// Bigint
+// let number7 = Bigint(575765676586756568657868);
+// Object
+// 1. Object
+// 2. Array
+// 3. Date
+// Object
+// const firstName = "Joy";
+// const lastName = "Ikwe";
+// const age = 40;
+// key: value
+const person = {
+    firstName: "Joy",
+    lastName: "Ikwe",
+    age: 40
+}
+console.log('person', person);
+console.log('firstName', person.firstName, person.lastName, person.age);
+console.log(`My name is ${person.firstName} ${person.lastName} and I am $
+{person.age} years old.`)
+const { firstName, lastName, age } = person;
+console.log('firstName', firstName);
+// Array
+const names = ["Ileri", "Hannah", "Feyi", "Teslim", "Divine", "Joy", "Iseoluwa",
+    "Ayomide"];
+// How to access values from an array
+// Create an array whose values are objects containing the first and last names
+// of everyone in this class.
+// Date
+const today = new Date();
+console.log('today', today);
+// Array methods
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 21, 23, 54, 32, 53, 523, 89, 2, 28, 99, 34,
+    89, 94, 893, 89823, 83, 823, 8329, 2983, 839, 9023093, 9893, 9983, 8322];
+// Length
+console.log('length', numbers.length);
+// console.log(numbers[2]);
+// console.log(numbers[8]);
+console.log(numbers[numbers.length - 1]);
+console.log(numbers[33 - 1]);
+console.log(numbers[32]);
+// Pop
+console.log(numbers.pop());
+console.log(numbers);
+// Push
+numbers.push(345, 456, 789);
+console.log('numbers', numbers)
+// Shift
+console.log(numbers.shift())
+console.log(numbers)
+// Unshift
+numbers.unshift(789, 'Divine');
+console.log('numbers', numbers);
+let firstNames = [
+    "Aaron",
+    "Aisha",
+    "Ajisafe",
+    "Undivine",
+    "Zainab",
+    "Zainab",
+    "Iseoluwa",
+    "Zainab",
+    "ileri",
+    "Hannah",
+    "Damilola",
+    "Hannah",
+    "Joy",
+    "Miracle",
+    "Hannah",
+    "Ayomide"
+];
+// Filter
+let ex = firstNames.filter((firstName) => {
+    return (
+        firstName.startsWith("A") && firstName.endsWith("e") || firstName.length
+        === 3
+    );
+});
+console.log('firstNames', firstNames)
+console.log('ex', ex);
+// find
+let startsWithA = firstNames.find((firstName) => {
+    return firstName.startsWith("A");
+});
+let startsWithB = firstNames.find(firstName => firstName.startsWith("A"));
+console.log('startsWithA', startsWithA)
+// some
+let startsWith = firstNames.some((firstName) => {
+    return firstName.startsWith("Z");
+});
+console.log("startsWith", startsWith);
+// includes
+let includes = firstNames.includes("Aaron");
+console.log("includes", includes);
+// reverse
+// let integers = [3, 4, 5, 6, 7];
+// integers.reverse();
+// console.log(integers);
+// map
+let integers = [3, 4, 5, 6, 7];
+let integersPlusTwo = integers.map((integer) => {
+    return integer + 2
+});
+console.log('integersPlusTwo', integersPlusTwo);
+let emailAddresses = firstNames.map((firstName) => {
+    return `${firstName}@outlook.com`
+});
+console.log('emailAddresses', emailAddresses);
+// forEach
+let eachName = firstNames.forEach((firstName) => {
+    console.log(`${firstName}@outlook.com`);
+});
+// console.log('eachName', eachName);
+// map
+// forEach
+// Conditional Statements using if, else...if and else
+//conditional statement
+let Age = 12;
+
+if (Age < 13) {
+    console.log("You are a child.");
+} else if (Age > 18) {
+    console.log("You are a teenager.");
+} else {
+    console.log("You are an adult.");
+}
+
+
+// switch
+
+let ages = 21;
+
+switch (true) {
+    case (age < 18):
+        console.log("You are not allowed to drink");
+        break;
+
+    case (18 <= age < 21):
+        console.log("You fit drink don't worry");
+        break;
+
+    case (age >= 21):
+        console.log("You can do drugs sef");
+        break;
+
+    // case 60:
+    //     console.log("Your score is 60");
+    //     break;
+
+    default:
+        break;
+}
+
+
+
+// Switch 
+// Loops
+// Functions 
